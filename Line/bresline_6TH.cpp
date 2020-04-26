e9ca11d33df4fb9f2065dd3017d98116a6078ae6
Tieu Lac <namkhmt2@gmail.com>
2020-04-26 20:51:11 +0700

    Them chu thich he so goc m.

--- Line/bresline_6TH.cpp
+++ Line/bresline_6TH.cpp
@@ -1,26 +1,26 @@
 #include <graphics.h>
 
-void bresline1(int xa, int ya, int xb, int yb);
-void bresline2(int xa, int ya, int xb, int yb);
-void bresline3(int xa, int ya, int xb, int yb);
-void bresline4(int xa, int ya, int xb, int yb);
-void bresline5(int xa, int ya, int xb, int yb);
-void bresline6(int xa, int ya, int xb, int yb);
+void bresline1(int xa, int ya, int xb, int yb);//doc
+void bresline2(int xa, int ya, int xb, int yb);//ngang
+void bresline3(int xa, int ya, int xb, int yb);//0<m<=1
+void bresline4(int xa, int ya, int xb, int yb);//-1<=m<0
+void bresline5(int xa, int ya, int xb, int yb);//m>1
+void bresline6(int xa, int ya, int xb, int yb);//m<-1
 
 int main(){
 	initwindow(640, 480);
 	setcolor(GREEN);
-	
+
 	bresline1(100,200,300,200);//TH1 if ya==yb
 	bresline2(200,100,200,300);//TH2 else if xa==xb
 	bresline3(300,250,100,150);//TH3 else if dx>dy -> toa do cung nho hon hoac lon hon
 	bresline4(100,250,300,150);//TH4 else if dx>dy -> else
 	bresline5(100,100,200,300);//TH5 else -> toa do cung nho hon hoac lon hon
 	bresline6(200,300,300,100);//TH6 else -> else
-	
+
 	getch();
 	closegraph();
-	
+
 	return 0;
 }
 
@@ -47,7 +47,7 @@ void bresline3(int xa, int ya, int xb, int yb){
 		std::swap(xa,xb);
 		std::swap(ya,yb);
 	}
-	
+
 	int dx=xb-xa, dy=yb-ya;
 	int const1 = 2*dy;
 	int const2 = 2*dy - 2*dx;
@@ -73,7 +73,7 @@ void bresline4(int xa, int ya, int xb, int yb){
 		std::swap(xa,xb);
 		std::swap(ya,yb);
 	}
-	
+
 	int dx=xb-xa, dy=yb-ya;
 	int const1 = 2*dy;
 	int const2 = 2*dy + 2*dx; // khac 3 cho nay
@@ -98,7 +98,7 @@ void bresline5(int xa, int ya, int xb, int yb){
 		std::swap(xa,xb);
 		std::swap(ya,yb);
 	}
-	
+
 	int dx=xb-xa, dy=yb-ya;
 	int const1 = 2*dx;
 	int const2 = 2*dx - 2*dy;
@@ -124,7 +124,7 @@ void bresline6(int xa, int ya, int xb, int yb){
 		std::swap(xa,xb);
 		std::swap(ya,yb);
 	}
-	
+
 	int dx=xb-xa, dy=yb-ya;
 	int const1 = 2*dx;
 	int const2 = 2*dx + 2*dy; // khac 5 cho nay
